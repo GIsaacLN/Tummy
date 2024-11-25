80a62d214d64c3a4b4a79ec2a682c8f6a4cb9b95
Isaac Lopez <isaac.lopez.1b@gmail.com>
2024-11-25 16:07:27 -0600

    Initial Commit

---
+++ Tummy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tummy
+//
+//  Created by Gustavo Isaac Lopez Nunez on 25/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tummy/TummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TummyApp.swift
+//  Tummy
+//
+//  Created by Gustavo Isaac Lopez Nunez on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
