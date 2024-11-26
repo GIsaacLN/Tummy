c25183401173097812b2b628d32c35c923174d80
Isaac Lopez <isaac.lopez.1b@gmail.com>
2024-11-26 12:02:47 -0600

    Statistics

---
+++ Tummy/DayStatisticsView.swift
@@ -0,0 +1,54 @@
+//
+//  DayStatisticsView.swift
+//  Tummy
+//
+//  Created by Gustavo Isaac Lopez Nunez on 26/11/24.
+//
+
+import SwiftUI
+
+struct DayStatisticsView: View {
+    let diaSemana: String
+    let diaNum: Int
+    let valor: Bool
+    let isToday: Bool
+
+    var body: some View {
+        VStack{
+            Text(diaSemana)
+                .font(.system(size: 13, weight: .bold))
+                .foregroundColor(.black)
+                .frame(maxWidth: .infinity)
+            
+            VStack {
+                ZStack {
+                    if valor {
+                        Image(systemName: "checkmark")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 20)
+                            .font(.system(size: 20, weight: .bold))
+                            .foregroundStyle(.white)
+                    }
+                }
+            }
+            .frame(width: 40, height: 70)
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            Text("\(diaNum)")
+                .font(.system(size: 16, weight: .bold))
+                .foregroundColor(.gray)
+                .frame(maxWidth: .infinity)
+        }
+        .padding(0)
+    }
+}
+
+#Preview {
+    DayStatisticsView(diaSemana: "L", diaNum: 10, valor: true, isToday: false)
+}
+
+#Preview {
+    DayStatisticsView(diaSemana: "M", diaNum: 11, valor: false, isToday: true)
+}
